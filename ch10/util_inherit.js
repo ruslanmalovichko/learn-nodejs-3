6814961113636f7be100e66958f80a114b53e28a
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2022-02-03 10:51:53 +0200

    Read a book

--- ch10/util_inherit.js
+++ ch10/util_inherit.js
@@ -1,16 +1,27 @@
 var util = require("util");
-var events = require("events");
-function Writer() {
-  events.EventEmitter.call(this);
+// var events = require("events");
+const EventEmitter = require('events').EventEmitter;
+
+// function Writer() {
+//   events.EventEmitter.call(this);
+// }
+// util.inherits(Writer, events.EventEmitter);
+
+class Writer extends EventEmitter {
+
 }
-util.inherits(Writer, events.EventEmitter);
+
 Writer.prototype.write = function(data) {
   this.emit("data", data);
 };
 var w = new Writer();
-console.log(w instanceof events.EventEmitter);
-console.log(Writer.super_ === events.EventEmitter);
+// console.log(w instanceof events.EventEmitter);
+console.log(w instanceof EventEmitter);
+// console.log(Writer.super_ === events.EventEmitter);
+console.log(Writer.super_ === EventEmitter);
 w.on("data", function(data) {
     console.log('Received data: "' + data + '"');
 });
 w.write("Some Data!");
+console.log(util.inspect(w));
+
