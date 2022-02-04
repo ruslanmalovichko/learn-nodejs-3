0f693c04e6ebc24522ac377e4b1934fab03ed647
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2022-02-04 17:54:08 +0200

    Read a book

--- ch13/db_connect_object.js
+++ ch13/db_connect_object.js
@@ -1,31 +1,35 @@
-var MongoClient = require('mongodb').MongoClient,
-    Server = require('mongodb').Server;
-var client = new MongoClient();
-client.connect('mongodb://localhost:27017', 
-		{poolSize: 5, reconnectInterval: 500, },
-function(err, db) {
-  if (err){
-    console.log("Connection Failed Via Client Object.");
-  } else {
-    var db = db.db("testDB");
-    if (db){
-      console.log("Connected Via Client Object . . .");
-      db.authenticate("dbadmin", "test", function(err, results){
-        if (err){
-          console.log("Authentication failed . . .");
-          db.close();
-          console.log("Connection closed . . .");
-        }else {
-          console.log("Authenticated Via Client Object . . .");
-          db.logout(function(err, result) {
-            if(!err){
-              console.log("Logged out Via Client Object . . .");
-            }
-            db.close();
-            console.log("Connection closed . . .");
-          });
-        }        
-      });
-    }
+// const { MongoClient } = require("mongodb");
+import { MongoClient } from 'mongodb'
+
+// Replace the following with values for your environment.
+const username = encodeURIComponent("dbadmin");
+const password = encodeURIComponent("test");
+// const clusterUrl = "localhost:27017";
+const clusterUrl = "localhost";
+const authMechanism = "DEFAULT";
+// Replace the following with your MongoDB deployment's connection string.
+// const uri = `mongodb+srv://${username}:${password}@${clusterUrl}/?authMechanism=${authMechanism}`;
+const uri = `mongodb://${username}:${password}@${clusterUrl}/?authMechanism=${authMechanism}`;
+// Create a new MongoClient
+const client = new MongoClient(uri);
+// Function to connect to the server
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Establish and verify connection
+    const ping = await client.db("testDB").command({ ping: 1 });
+    console.log(ping)
+    console.log("Connected successfully to server");
+
+    const db = client.db("testDB");
+    const collection = db.collection('newCollection');
+    const findResult = await collection.find({}).toArray();
+    console.log('Found documents =>', findResult);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
   }
-});
+}
+run().catch(console.dir);
+
