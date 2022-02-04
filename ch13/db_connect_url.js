c7e64ab73dc01b36ddd0de95e539713f16289828
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2022-02-04 16:46:09 +0200

    Read a book

--- ch13/db_connect_url.js
+++ ch13/db_connect_url.js
@@ -1,19 +1,27 @@
-var MongoClient = require('mongodb').MongoClient,
-    Server = require('mongodb').Server;
-var client = new MongoClient();
-client.connect('mongodb://dbadmin:test@localhost:27017/testDB', 
-		       {poolSize: 5, reconnectInterval: 500 },
-function(err, db) {
-  if (err){
-    console.log("Connection Failed Via Client Object.");
-  } else {
-    console.log("Connected Via Client Object . . .");
-    db.logout(function(err, result) {
-      if(!err){
-        console.log("Logged out Via Client Object . . .");
-      }
-      db.close();
-          console.log("Connection closed . . .");
-    });
-  }
-});
+const { MongoClient } = require('mongodb');
+
+// Connection URL
+const url = 'mongodb://dbadmin:test@localhost:27017';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'testDB';
+const options = {poolSize: 5, reconnectInterval: 500};
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('newCollection');
+  const findResult = await collection.find({}).toArray();
+  console.log('Found documents =>', findResult);
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
+
