2bbb3fd7f5958cd7d0a0e67557199140808c6a7c
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2022-02-07 16:32:36 +0200

    Read a book

--- ch14/doc_find.js
+++ ch14/doc_find.js
@@ -1,24 +1,46 @@
-var MongoClient = require('mongodb').MongoClient;
-MongoClient.connect("mongodb://localhost/", function(err, db) {
-  var myDB = db.db("astro");
-  myDB.collection("nebulae", function(err, nebulae){
-    nebulae.find(function(err, items){
-      items.toArray(function(err, itemArr){
-        console.log("Document Array: ");
-        console.log(itemArr);
-      });
-    });
-    nebulae.find(function(err, items){
-      items.each(function(err, item){
-        if(item){
-          console.log("Singular Document: ");
-          console.log(item);
-        }
-      });
-    });
-    nebulae.findOne({type:'planetary'}, function(err, item){
-      console.log("Found One: ");
+import { MongoClient } from 'mongodb'
+// Connection URL
+const url = 'mongodb://dbadmin:test@localhost:27017';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'astro';
+
+// Collection Name
+const collectionName = 'nebulae';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+
+  let newDB = client.db(dbName);
+
+  let collection = newDB.collection(collectionName);
+
+  let items = collection.find();
+  let itemArr = await items.toArray();
+  console.log("Document Array: ");
+  console.log(itemArr);
+
+  items = collection.find();
+
+  await items.forEach(function(item) {
+    if (item) {
+      console.log("Singular Document: ");
       console.log(item);
-    });
+    }
   });
-});
+
+  let item = await collection.findOne({type:'planetary'});
+  console.log("Found One: ");
+  console.log(item);
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
+
