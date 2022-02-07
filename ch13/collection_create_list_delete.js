d366108bbec0ecb0789692771d854d92ec53db38
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2022-02-07 14:12:48 +0200

    Read a book

--- ch13/collection_create_list_delete.js
+++ ch13/collection_create_list_delete.js
@@ -1,21 +1,44 @@
-var MongoClient = require('mongodb').MongoClient;
-MongoClient.connect("mongodb://localhost/", function(err, db) {
-  var newDB = db.db("newDB");
-  newDB.collections(function(err, collectionNames){
-    console.log("Initial collections: ");
-    console.log(collectionNames);
-    newDB.createCollection("newCollection", function(err, collection){
-      newDB.collections(function(err, collectionNames){
-        console.log("Collections after creation: ");
-        console.log(collectionNames);
-        newDB.dropCollection("newCollection", function(err, results){
-          newDB.collections(function(err, collectionNames){
-            console.log("Collections after deletion: ");
-            console.log(collectionNames);
-            db.close();
-          });
-        });
-      });
-    });
-  });
-});
+import { MongoClient } from 'mongodb'
+// Connection URL
+const url = 'mongodb://dbadmin:test@localhost:27017';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'newDB';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+
+  await client.db(dbName).dropDatabase();
+
+  let newDB = client.db(dbName);
+
+  let collectionNames = await newDB.collections();
+
+  console.log("Initial collections: ");
+  console.log(collectionNames);
+
+  let collection = await newDB.createCollection("newCollection");
+
+  collectionNames = await newDB.collections();
+
+  console.log("Collections after creation: ");
+  console.log(collectionNames);
+
+  let results = await newDB.dropCollection("newCollection");
+
+  collectionNames = await newDB.collections();
+
+  console.log("Collections after deletion: ");
+  console.log(collectionNames);
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
+
