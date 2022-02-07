e074daee49afc4aea9c78748c319fb81e3a146cb
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2022-02-07 14:16:35 +0200

    Read a book

--- ch13/collection_stats.js
+++ ch13/collection_stats.js
@@ -1,10 +1,27 @@
-var MongoClient = require('mongodb').MongoClient;
-MongoClient.connect("mongodb://localhost/", function(err, db) {
-  var newDB = db.db("newDB");
-  newDB.createCollection("newCollection", function(err, collection){
-    collection.stats(function(err, stats){
-      console.log(stats);
-      db.close();
-    });
-  });
-});
+import { MongoClient } from 'mongodb'
+// Connection URL
+const url = 'mongodb://dbadmin:test@localhost:27017';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'newDB';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+
+  await client.db(dbName).dropDatabase();
+
+  let newDB = client.db(dbName);
+  let collection = await newDB.createCollection("newCollection");
+  let stats = await collection.stats();
+  console.log(stats.storageSize);
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
+
