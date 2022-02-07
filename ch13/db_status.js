be2fab31a0dedf636a6c82753213b0d732f09a17
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2022-02-07 13:56:32 +0200

    Read a book

--- ch13/db_status.js
+++ ch13/db_status.js
@@ -1,8 +1,28 @@
-var MongoClient = require('mongodb').MongoClient;
-MongoClient.connect("mongodb://localhost/test", function(err, db) {
-  var adminDB = db.admin();
-  adminDB.serverStatus(function(err, status){
-    console.log(status);
-    db.close();
-  });
-});
+import { MongoClient } from 'mongodb'
+// Connection URL
+const url = 'mongodb://dbadmin:test@localhost:27017';
+const client = new MongoClient(url);
+
+// Database Name
+// const dbName = 'newDB';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db();
+
+  // await client.db(dbName).dropDatabase();
+
+  let adminDB = db.admin();
+  let serverStatus = await adminDB.serverStatus();
+  console.log(serverStatus.connections);
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
+
