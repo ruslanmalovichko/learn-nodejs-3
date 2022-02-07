0b3dd6787965c0ed6c513e508966c73768a084fd
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2022-02-07 16:12:02 +0200

    Read a book

--- ch14/doc_add.js
+++ ch14/doc_add.js
@@ -1,21 +1,46 @@
-var MongoClient = require('mongodb').MongoClient;
-function addObject(collection, object){
-  collection.insert(object, function(err, result){
-    if(!err){
-      console.log("Inserted : ");
-      console.log(result);
-    }
-  });
+async function addObject(collection, object){
+  let result = await collection.insertOne(object);
+  console.log("Inserted : ");
+  console.log(result);
 }
-MongoClient.connect("mongodb://localhost/", function(err, db) {  
-  var myDB = db.db("astro");
-  myDB.dropCollection("nebulae");
-  myDB.createCollection("nebulae", function(err, nebulae){
-    addObject(nebulae, {ngc:"NGC 7293", name:"Helix",
-      type:"planetary",location:"Aquila"});
-    addObject(nebulae, {ngc:"NGC 6543", name:"Cat's Eye",
-      type:"planetary",location:"Draco"});
-    addObject(nebulae, {ngc:"NGC 1952", name: "Crab",
-      type:"supernova",location:"Taurus"});
-  });
-});
+
+import { MongoClient } from 'mongodb'
+// Connection URL
+const url = 'mongodb://dbadmin:test@localhost:27017';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'astro';
+
+// Collection Name
+const collectionName = 'nebulae';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+
+  await client.db(dbName).dropDatabase();
+
+  let newDB = client.db(dbName);
+
+  let collection = await newDB.createCollection(collectionName);
+
+  await addObject(collection, {ngc:"NGC 7293", name:"Helix", type:"planetary",location:"Aquila"});
+  await addObject(collection, {ngc:"NGC 6543", name:"Cat's Eye", type:"planetary",location:"Draco"});
+  await addObject(collection, {ngc:"NGC 1952", name: "Crab", type:"supernova",location:"Taurus"});
+
+  newDB = client.db(dbName);
+
+  let documents = newDB.collection(collectionName).find();
+
+  console.log(await documents.toArray());
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
+
