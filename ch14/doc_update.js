d6e37d2534b1aebf5b0635643cdb0132efdf9999
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2022-02-07 17:50:02 +0200

    Read a book

--- ch14/doc_update.js
+++ ch14/doc_update.js
@@ -1,24 +1,46 @@
-var MongoClient = require('mongodb').MongoClient;
-MongoClient.connect("mongodb://localhost/", function(err, db) {
-  var myDB = db.db("astro");
-  myDB.collection("nebulae", function(err, nebulae){
-    nebulae.find({type:"planetary"}, function(err, items){
-      items.toArray(function(err, itemArr){
-        console.log("Before Update: ");
-        console.log(itemArr);
-        nebulae.update({type:"planetary", $isolated:1}, 
-                       {$set:{type:"Planetary", updated:true}},
-                       {upsert:false, multi:true, w:1}, 
-                       function(err, results){
-          nebulae.find({type:"Planetary"}, function(err, items){
-            items.toArray(function(err, itemArr){
-              console.log("After Update: ");
-              console.log(itemArr);
-              db.close();
-            });
-          });
-        });
-      });
-    });
-  });
-});
+import { MongoClient } from 'mongodb'
+// Connection URL
+const url = 'mongodb://dbadmin:test@localhost:27017';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'astro';
+
+// Collection Name
+const collectionName = 'nebulae';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+
+  let newDB = client.db(dbName);
+
+  let collection = newDB.collection(collectionName);
+
+  let items = collection.find({type:"planetary"});
+
+  let itemArr = await items.toArray();
+
+  console.log("Before Update: ");
+  console.log(itemArr);
+  let results = await collection.updateMany(
+    {type:"planetary"},
+    {$set:{type:"Planetary", updated:true}},
+    {upsert:false}
+  );
+
+  collection = newDB.collection(collectionName);
+  items = collection.find({type:"Planetary"});
+  itemArr = await items.toArray();
+  console.log("After Update: ");
+  console.log(itemArr);
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
+
