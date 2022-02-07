1eeb67e62a506a2dec29fef7d4a7b0be9c95e006
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2022-02-07 12:45:55 +0200

    Read a book

--- ch13/db_create_list_delete.js
+++ ch13/db_create_list_delete.js
@@ -1,36 +1,51 @@
-var MongoClient = require('mongodb').MongoClient;
-MongoClient.connect("mongodb://localhost/", function(err, db) {
+import { MongoClient } from 'mongodb'
+// Connection URL
+const url = 'mongodb://dbadmin:test@localhost:27017';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'newDB';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db();
+
+  await client.db(dbName).dropDatabase();
+
   var adminDB = db.admin();
-  adminDB.listDatabases(function(err, databases){
-    console.log("Before Add Database List: ");
-    console.log(databases);
-  });
-  var newDB = db.db("newDB");
-  newDB.createCollection("newColleciton", function(err, collection){
-    if(!err){
-      console.log("New Database and Collection Created");
-      adminDB.listDatabases(function(err, databases){
-        console.log("After Add Database List: ");
-        console.log(databases);
-        db.db("newDB").dropDatabase(function(err, results){
-          if(!err){
-            console.log("Database dropped.");
-            setTimeout(function() {            
-              adminDB.listDatabases(function(err, results){
-                var found = false;
-                for(var i = 0; i < results.databases.length; i++) {
-                  if(results.databases[i].name == "newDB") found = true;
-                }
-                if (!found){
-                  console.log("After Delete Database List: ");
-                  console.log(results);
-                }
-                db.close();
-              });
-            }, 15000);
-          }
-        });
-      });
-    }
-  });
-});
+  let listDatabases = await adminDB.listDatabases();
+  console.log("Before Add Database List: ");
+  console.log(listDatabases);
+
+  var newDB = client.db(dbName);
+  await newDB.createCollection("newCollection");
+  console.log("New Database and Collection Created");
+
+  listDatabases = await adminDB.listDatabases();
+  console.log("After Add Database List: ");
+  console.log(listDatabases);
+
+  await client.db(dbName).dropDatabase();
+
+  console.log("Database dropped.");
+
+  listDatabases = await adminDB.listDatabases();
+  var found = false;
+  for (var i = 0; i < listDatabases.databases.length; i++) {
+    if (listDatabases.databases[i].name == dbName) found = true;
+  }
+  if (!found){
+    console.log("After Delete Database List: ");
+    console.log(listDatabases);
+  }
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
+
