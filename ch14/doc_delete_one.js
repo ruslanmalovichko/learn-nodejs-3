c6dbb143a8ce12d2b78a9ad88d3c04c34709cf68
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2022-02-08 14:37:08 +0200

    Read a book

--- ch14/doc_delete_one.js
+++ ch14/doc_delete_one.js
@@ -1,23 +1,51 @@
-var MongoClient = require('mongodb').MongoClient;
-MongoClient.connect("mongodb://localhost/", function(err, db) {
-  var myDB = db.db("astro");
-  myDB.collection("nebulae", function(err, nebulae){
-    nebulae.find(function(err, items){
-      items.toArray(function(err, itemArr){
-        console.log("Before Delete: ");
-        console.log(itemArr);
-        nebulae.findAndRemove({type:"planetary"}, [['name', 1]], 
-                              {w:1}, function(err, results){
-          console.log("Deleted " + results + " documents.");
-          nebulae.find(function(err, items){
-            items.toArray(function(err, itemArr){
-              console.log("After Delete: ");
-              console.log(itemArr);
-              db.close();
-            });
-          });
-        });
-      });
-    });
-  });
-});
+import { MongoClient } from 'mongodb'
+// Connection URL
+const url = 'mongodb://dbadmin:test@localhost:27017';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'astro';
+
+// Collection Name
+const collectionName = 'nebulae';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+
+  let newDB = client.db(dbName);
+
+  let collection = newDB.collection(collectionName);
+
+  let items = collection.find();
+  let itemArr = await items.toArray();
+  console.log("Before Delete: ");
+  console.log(itemArr);
+
+  let results = await collection.findOneAndDelete(
+    {type:"planetary"},
+    {
+      sort: [['name', 1]],
+    }
+  );
+
+  console.log("Deleted ");
+  console.log(results);
+  console.log(" documents.");
+
+  items = collection.find();
+
+  itemArr = await items.toArray();
+
+  console.log("After Delete: ");
+  console.log(itemArr);
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
+
