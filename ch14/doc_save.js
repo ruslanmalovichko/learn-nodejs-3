903aea39fcdf39dddaefc8c841cabc5bbd403c3d
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2022-02-08 11:36:22 +0200

    Read a book

--- ch14/doc_save.js
+++ ch14/doc_save.js
@@ -1,18 +1,36 @@
-var MongoClient = require('mongodb').MongoClient;
-MongoClient.connect("mongodb://localhost/", function(err, db) {
-  var myDB = db.db("astro");
-  myDB.collection("nebulae", function(err, nebulae){
-    nebulae.findOne({type: "supernova"}, function(err, item){
-      console.log("Before Save: ");
-      console.log(item);
-      item.info = "Some New Info";
-      nebulae.save(item, {w:1}, function(err, results){
-        nebulae.findOne({_id:item._id}, function(err, savedItem){
-          console.log("After Save: ");
-          console.log(savedItem);
-          db.close();
-        });
-      });
-    });
-  });
-});
+import { MongoClient } from 'mongodb'
+// Connection URL
+const url = 'mongodb://dbadmin:test@localhost:27017';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'astro';
+
+// Collection Name
+const collectionName = 'nebulae';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+
+  let newDB = client.db(dbName);
+
+  let collection = newDB.collection(collectionName);
+  let item = await collection.findOne({type: "supernova"})
+  console.log("Before Save: ");
+  console.log(item);
+  item.info = "Some New Info";
+  await collection.replaceOne({type: "supernova"}, item);
+  let savedItem = await collection.findOne({_id:item._id});
+  console.log("After Save: ");
+  console.log(savedItem);
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
+
