86e3da7c52e0b2650b71d88b82ea8757b279323b
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2022-02-09 15:54:24 +0200

    Read a book

--- ch15/doc_limit.js
+++ ch15/doc_limit.js
@@ -1,29 +1,117 @@
-var MongoClient = require('mongodb').MongoClient;
-MongoClient.connect("mongodb://localhost/", function(err, db) {
-  var myDB = db.db("words");
-  myDB.collection("word_stats", limitFind);
-  setTimeout(function(){
-    db.close();
-  }, 3000);
-});
-function displayWords(msg, cursor, pretty){
-  cursor.toArray(function(err, itemArr){
-    console.log("\n"+msg);
-    var wordList = [];
-    for(var i=0; i<itemArr.length; i++){
-      wordList.push(itemArr[i].word);
-    }
-    console.log(JSON.stringify(wordList, null, pretty));
-  });
+async function displayWords(msg, cursor, pretty){
+  let itemArr = await cursor.toArray();
+  console.log("\n"+msg);
+  var wordList = [];
+  for(var i=0; i<itemArr.length; i++){
+    wordList.push(itemArr[i].word);
+  }
+  console.log(JSON.stringify(wordList, null, pretty));
 }
-function limitFind(err, words){
-  words.count({first:'p'}, function(err, count){
-    console.log("Count of words starting with p : " + count);
+
+async function limitFind(words){
+  let count = await words.count({
+    word: {$regex: '^p'}
   });
-  words.find({first:'p'}, function(err, cursor){
-    displayWords("Words starting with p : ", cursor);
+  console.log("Count of words starting with p : " + count);
+
+  let cursor = await words.find({
+    // word: {$regex: '^p'}
   });
-  words.find({first:'p'}, {limit:5}, function(err, cursor){
-    displayWords("Limiting words starting with p : ", cursor);
-  });
+  await displayWords("Words starting with p : ", cursor);
+
+  // cursor = await words.find({
+  //   // word: {$regex: '^p'},
+  // },
+  // {limit:5}
+  // );
+
+  cursor = await words.find({
+    // word: {$regex: '^p'},
+  }).limit(5);
+  await displayWords("Limiting words starting with p : ", cursor);
 }
+
+import { MongoClient } from 'mongodb'
+// Connection URL
+const url = 'mongodb://dbadmin:test@localhost:27017';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'words';
+
+// Collection Name
+const collectionName = 'word_stats';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+
+  // await client.db(dbName).dropDatabase();
+
+  let newDB = client.db(dbName);
+  let collection = newDB.collection(collectionName);
+
+  let results = await collection.updateMany(
+    {type:'words',word: 'ruslan'},
+    {$set:{type:'words',word: 'ruslan'}},
+    {upsert:true}
+  );
+
+  results = await collection.updateMany(
+    {type:'words',word: 'aruslan'},
+    {$set:{type:'words',word: 'aruslan'}},
+    {upsert:true}
+  );
+
+  results = await collection.updateMany(
+    {type:'words',word: 'aruslani'},
+    {$set:{type:'words',word: 'aruslani'}},
+    {upsert:true}
+  );
+
+  results = await collection.updateMany(
+    {type:'words',word: 'bruslan'},
+    {$set:{type:'words',word: 'bruslan'}},
+    {upsert:true}
+  );
+  results = await collection.updateMany(
+    {type:'words',word: 'ruslanruslan'},
+    {$set:{type:'words',word: 'ruslanruslan'}},
+    {upsert:true}
+  );
+
+  results = await collection.updateMany(
+    {type:'words',word: 'ruslanruslanr'},
+    {$set:{type:'words',word: 'ruslanruslanr'}},
+    {upsert:true}
+  );
+
+  results = await collection.updateMany(
+    {type:'words',word: '@ruslan'},
+    {$set:{type:'words',word: '@ruslan'}},
+    {upsert:true}
+  );
+
+  results = await collection.updateMany(
+    {type:'words',word: 'pruslan'},
+    {$set:{type:'words',word: 'pruslan'}},
+    {upsert:true}
+  );
+
+  // await findItems(collection);
+  await limitFind(collection);
+
+  let items = collection.find();
+  let itemArr = await items.toArray();
+  console.log("Document Array: ");
+  console.log(itemArr);
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
+
