3644deecdf620ca50f1f9474d3cc3d4131a399d6
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2022-02-11 14:32:28 +0200

    Read a book

--- ch16/mongoose_find.js
+++ ch16/mongoose_find.js
@@ -1,33 +1,24 @@
-var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost/words');
-var wordSchema = require('./word_schema.js').wordSchema;
-var Words = mongoose.model('Words', wordSchema);
-setTimeout(function(){
-  mongoose.disconnect();
-}, 3000);
-mongoose.connection.once('open', function(){
-  var query = Words.count().where('first').in(['a', 'e', 'i', 'o', 'o']);
-  query.where('last').in(['a', 'e', 'i', 'o', 'o']);
-  query.exec(function(err, count){
-    console.log("\nThere are " + count +
-                " words that start and end with a vowel");
-  });
-  query.find().limit(5).sort({size:-1});
-  query.exec(function(err, docs){
-    console.log("\nLongest 5 words that start and end with a vowel: ");
-    for (var i in docs){
-      console.log(docs[i].word);
-    }
-  });
-  query = Words.find();
-  query.mod('size',2,0);
-  query.where('size').gt(6);
-  query.limit(10);
-  query.select({word:1, size:1});
-  query.exec(function(err, docs){
-    console.log("\nWords with even lengths and longer than 5 letters: ");
-    for (var i in docs){
-      console.log(JSON.stringify(docs[i]));
-    }
-  });
-});
+import mongoose from 'mongoose';
+
+import Words from './models/words.js';
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/words');
+  let count = await Words.find({word: {$regex: '(^a|^e|^i|^o|^u).*?(a$|e$|i$|o$|u$)'}}).count();
+  console.log("\nThere are " + count + " words that start and end with a vowel");
+
+  let docs = await Words.find({word: {$regex: '(^a|^e|^i|^o|^u).*?(a$|e$|i$|o$|u$)'}}).limit(5).sort({ size: -1 });
+  console.log("\nLongest 5 words that start and end with a vowel: ");
+  for (var i in docs){
+    console.log(docs[i].word);
+  }
+
+  docs = await Words.find().mod('size',2,0).where('size').gt(6).limit(10).select({word:1, size:1});
+  console.log("\nWords with even lengths and longer than 5 letters: ");
+  for (var i in docs){
+    console.log(JSON.stringify(docs[i]));
+  }
+}
+
