5d13b9eecc9fa094f6089e572ec19ced5f1ce825
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2022-02-11 14:33:00 +0200

    Read a book

---
+++ ch16/models/words.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+const { model, Schema } = mongoose;
+const Mixed = Schema.Types.Mixed;
+const options = { collection: 'word_stats' };
+
+const wordSchema = new Schema({
+  word: {type: String, index: true, required:true, unique: true},
+  // word: {type: Number, index: true, required:true, unique: true},
+  first: {type: String, index: true},
+  last: String,
+  size: Number,
+  letters: [String],
+  stats: {vowels:Number, consonants:Number},
+  charsets: [Mixed]
+},
+  options
+);
+
+wordSchema.methods.startsWith = function(letter){
+  return this.first === letter;
+};
+
+export default model('Words', wordSchema);
+
