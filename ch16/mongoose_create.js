d03213eb83a6eea276bc39cd16ff93f27e1f95f2
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2022-02-14 14:17:14 +0200

    Read a book

--- ch16/mongoose_create.js
+++ ch16/mongoose_create.js
@@ -1,33 +1,45 @@
-var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost/words');
-var wordSchema = require('./word_schema.js').wordSchema;
-var Words = mongoose.model('Words', wordSchema);
-mongoose.connection.once('open', function(){
-  var newWord1 = new Words({
+import mongoose from 'mongoose';
+
+import Words from './models/words.js';
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/words');
+  const newWord1 = new Words({
     word:'gratifaction',
-    first:'g', last:'n', size:12,
+    first:'g',
+    last:'n',
+    size:12,
     letters: ['g','r','a','t','i','f','c','o','n'],
     stats: {vowels:5, consonants:7}
   });
   console.log("Is Document New? " + newWord1.isNew);
-  newWord1.save(function(err, doc){
-    console.log("\nSaved document: " + doc);
-  });
-  var newWord2 = { word:'googled',
-    first:'g', last:'d', size:7,
+  let doc = await newWord1.save();
+  console.log("\nSaved document: " + doc);
+
+  let newWord2 = {
+    word:'googled',
+    first:'g',
+    last:'d',
+    size:7,
     letters: ['g','o','l','e','d'],
     stats: {vowels:3, consonants:4}
   };
-  var newWord3 = {
+  let newWord3 = {
     word:'selfie',
-    first:'s', last:'e', size:6,
+    first:'s',
+    last:'e',
+    size:6,
     letters: ['s','e','l','f','i'],
     stats: {vowels:3, consonants:3}
   };
-  Words.create([newWord2, newWord3], function(err){
-    for(var i=1; i<arguments.length; i++){
-      console.log("\nCreated document: " + arguments[i]);      
-    }
-    mongoose.disconnect();
+
+  let docs = await Words.create([newWord2, newWord3]);
+  // docs.forEach(function(item, i, arr) {
+  docs.forEach(function(item) {
+    console.log("\nCreated document: " + item);
   });
-});
+  await mongoose.disconnect();
+}
+
