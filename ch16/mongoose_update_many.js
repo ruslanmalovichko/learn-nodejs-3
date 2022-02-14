a50c732803c6a608a4608a72b896702f9105dae8
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2022-02-14 16:36:47 +0200

    Read a book

--- ch16/mongoose_update_many.js
+++ ch16/mongoose_update_many.js
@@ -1,24 +1,25 @@
-var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost/words');
-var wordSchema = require('./word_schema.js').wordSchema;
-var Words = mongoose.model('Words', wordSchema);
-mongoose.connection.once('open', function(){
-  Words.find({word:/grati.*/}, function(err, docs){
-    console.log("Before update: ");
-    for (var i in docs){
-      console.log(docs[i].word + " : " + docs[i].size);
-    }
-    var query = Words.update({}, {$set: {size: 0}});
-    query.setOptions({multi: true});
-    query.where('word').regex(/grati.*/);
-    query.exec(function(err, results){
-      Words.find({word:/grat.*/}, function(err, docs){
-        console.log("\nAfter update: ");
-        for (var i in docs){
-          console.log(docs[i].word + " : " + docs[i].size);
-        }
-        mongoose.disconnect();
-      });
-    });
-  });
-});
+import mongoose from 'mongoose';
+
+import Words from './models/words.js';
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/words');
+  let docs = await Words.find();
+  console.log("Before update: ");
+  for (var i in docs){
+    console.log(docs[i].word + " : " + docs[i].size);
+  }
+  const filter = { word: { $regex: /grati.*/ } };
+  await Words.updateMany(filter, {$set: {size: 0}});
+
+  docs = await Words.find({word:/grat.*/});
+  console.log("\nAfter update: ");
+  for (var i in docs){
+    console.log(docs[i].word + " : " + docs[i].size);
+  }
+
+  await mongoose.disconnect();
+}
+
