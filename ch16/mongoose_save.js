4b9974533b72843efc5f1a6634c01430f442cb3b
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2022-02-14 15:48:06 +0200

    Read a book

--- ch16/mongoose_save.js
+++ ch16/mongoose_save.js
@@ -1,23 +1,38 @@
-var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost/words');
-var wordSchema = require('./word_schema.js').wordSchema;
-var Words = mongoose.model('Words', wordSchema);
-mongoose.connection.once('open', function(){
-  var query = Words.findOne().where('word', 'book');
-  query.exec(function(err, doc){
-    console.log("Is Document New? " + doc.isNew);
-    console.log("\nBefore Save: ");
-    console.log(doc.toJSON());
-    doc.set('word','Book');
-    doc.set('first','B');
-    console.log("\nModified Fields: ");
-    console.log(doc.modifiedPaths());
-    doc.save(function(err){
-      Words.findOne({word:'Book'}, function(err, doc){
-        console.log("\nAfter Save: ");
-        console.log(doc.toJSON());
-        mongoose.disconnect();
-      });
-    });
+import mongoose from 'mongoose';
+
+import Words from './models/words.js';
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/words');
+  let doc;
+  const book = new Words({
+    word:'book',
+    first:'b',
+    last:'k',
+    size:4,
+    letters: ['b','o','o','k'],
+    stats: {vowels:2, consonants:2}
   });
-});
+  console.log("Is Document New? " + book.isNew);
+  doc = await book.save();
+  console.log("\nSaved document: " + doc);
+
+  doc = await Words.findOne().where('word', 'book');
+  console.log(doc);
+  console.log("Is Document New? " + doc.isNew);
+  console.log("\nBefore Save: ");
+  console.log(doc.toJSON());
+  doc.set('word','Book');
+  doc.set('first','B');
+  doc.set('letters',['B', 'o', 'o', 'k']);
+  console.log("\nModified Fields: ");
+  console.log(doc.modifiedPaths());
+  await doc.save();
+  doc = await Words.findOne({word:'Book'});
+  console.log("\nAfter Save: ");
+  console.log(doc.toJSON());
+  await mongoose.disconnect();
+}
+
