ac608a8b466b21be456dfa1d83a2688dd9753e37
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2022-02-15 16:30:12 +0200

    Read a book

--- ch16/models/words.js
+++ ch16/models/words.js
@@ -34,6 +34,40 @@ const wordSchema = new Schema({
   options
 );
 
+wordSchema.pre('init', async function() {
+  console.log('a new word is about to be initialized from the db');
+});
+
+wordSchema.pre('validate', async function() {
+  console.log('%s is about to be validated', this.word);
+});
+
+wordSchema.pre('save', async function() {
+  console.log('%s is about to be saved', this.word);
+  console.log('Setting size to %d', this.word.length);
+  this.size = this.word.length;
+});
+
+wordSchema.pre('remove', async function() {
+  console.log('%s is about to be removed', this.word);
+});
+
+wordSchema.post('init', async function(doc) {
+  console.log('%s has been initialized from the db', doc.word);
+});
+
+wordSchema.post('validate', async function(doc) {
+  console.log('%s has been validated', doc.word);
+});
+
+wordSchema.post('save', async function(doc) {
+  console.log('%s has been saved', doc.word);
+});
+
+wordSchema.post('remove', async function(doc) {
+  console.log('%s has been removed', doc.word);
+});
+
 wordSchema.methods.startsWith = function(letter){
   return this.first === letter;
 };
