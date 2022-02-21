e2f1c4e9cb25c68a4bdd26f1890ab90ad84c4724
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2022-02-21 16:59:33 +0200

    Read a book

--- ch18/express_send.js
+++ ch18/express_send.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var url = require('url');
-var app = express();
+import express from 'express';
+let app = express();
+
 app.listen(80);
+
 app.get('/', function (req, res) {
-  var response = '<html><head><title>Simple Send</title></head>' +
+  let response = '<html><head><title>Simple Send</title></head>' +
                  '<body><h1>Hello from Express</h1></body></html>';
   res.status(200);
   res.set({
@@ -11,11 +12,12 @@ app.get('/', function (req, res) {
     'Content-Length': response.length
   });
   res.send(response);
-  console.log('Response Finished? ' + res.finished);
+  console.log('Response Finished? ' + res.writableEnded);
   console.log('\nHeaders Sent: ');
-  console.log(res.headerSent);
+  console.log(res._header);
 });
 app.get('/error', function (req, res) {
   res.status(400);
   res.send("This is a bad request.");
 });
+
