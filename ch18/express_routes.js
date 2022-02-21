b196b81a1ed9103e76aa3c8725f8e05eebf71357
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2022-02-21 11:31:15 +0200

    Read a book

--- ch18/express_routes.js
+++ ch18/express_routes.js
@@ -1,37 +1,42 @@
-var express = require('express');
-var url = require('url');
-var app = express();
+import express from 'express';
+import url from 'url';
+let app = express();
+
 app.listen(80);
-app.get('/', function (req, res) {
+
+app.get('/', function(req, res) { // gets / only
   res.send("Get Index");
 });
-app.get('/find', function(req, res){
-  var url_parts = url.parse(req.url, true);
-  var query = url_parts.query; 
-  var response = 'Finding Book: Author: ' + query.author + 
-                  ' Title: ' + query.title;
-  console.log('\nQuery URL: ' + req.originalUrl);  
+
+app.get('/find', function(req, res) { // gets /find with parameters
+  let url_parts = url.parse(req.url, true);
+  let query = url_parts.query;
+  let response = 'Finding Book: Author: ' + query.author + ' Title: ' + query.title;
+  console.log('\nQuery URL: ' + req.originalUrl);
   console.log(response);
   res.send(response);
 });
-app.get(/^\/book\/(\w+)\:(\w+)?$/, function(req, res){
-  var response = 'Get Book: Chapter: ' + req.params[0] + 
-              ' Page: ' + req.params[1];
+
+app.get(/^\/book\/(\w+)\:(\w+)?$/, function(req, res) { // gets /book/*:*
+  let response = 'Get Book: Chapter: ' + req.params[0] + ' Page: ' + req.params[1];
   console.log('\nRegex URL: ' + req.originalUrl);
   console.log(response);
   res.send(response);
 });
-app.get('/user/:userid', function (req, res) {
-  var response = 'Get User: ' + req.param('userid');
+
+app.get('/user/:userid', function(req, res) { // gets /user/*
+  let response = 'Get User: ' + req.params.userid;
   console.log('\nParam URL: ' + req.originalUrl);
   console.log(response);
   res.send(response);
 });
-app.param('userid', function(req, res, next, value){
+
+app.param('userid', function(req, res, next, value) { // can not live without app.get. // gets /user/*
   console.log("\nRequest received with userid: " + value);
-  next();
+  next(); // without next it does not call app.get
 });
 
-//    /find?author=Brad&title=Node
-//    /book/12:15
-//    /user/4983
+// /find?author=Brad&title=Node
+// /book/12:15
+// /user/4983
+
