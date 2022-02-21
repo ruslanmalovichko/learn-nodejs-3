e1a982da8a7edcf96595cf5a377d41c49c0e6803
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2022-02-21 11:11:07 +0200

    Read a book

--- ch18/express_http_https.js
+++ ch18/express_http_https.js
@@ -4,10 +4,10 @@ import http from 'http';
 import fs from 'fs';
 let app = express();
 let options = {
-    host: '127.0.0.1',
-    key: fs.readFileSync('ssl/server.key'),
-    cert: fs.readFileSync('ssl/server.crt')
-  };
+  host: '127.0.0.1',
+  key: fs.readFileSync('ssl/server.key'),
+  cert: fs.readFileSync('ssl/server.crt')
+};
 http.createServer(app).listen(80);
 https.createServer(options, app).listen(443);
 app.get('/', function(req, res){
