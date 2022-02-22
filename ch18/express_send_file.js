e5f436dfabc6b282842df548dc9fa4888d7abcc3
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2022-02-22 14:07:40 +0200

    Read a book

--- ch18/express_send_file.js
+++ ch18/express_send_file.js
@@ -1,16 +1,17 @@
-var express = require('express');
-var url = require('url');
-var app = express();
+import express from 'express';
+
+let app = express();
+
 app.listen(80);
+
 app.get('/image', function (req, res) {
-  res.sendfile('arch.jpg', 
-               { maxAge: 24*60*60*1000,
-                 root: './views/'},
-               function(err){
+  res.sendFile('arch.jpg', { maxAge: 24*60*60*1000, root: './views/'}, function(err) {
     if (err){
       console.log("Error");
-    } else {
+    }
+    else {
       console.log("Success");
     }
   });
 });
+
