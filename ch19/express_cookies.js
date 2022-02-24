cdd0d5257ddace53ad51ab2ea3ca91e65586983a
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2022-02-24 10:44:25 +0200

    Read a book

--- ch19/express_cookies.js
+++ ch19/express_cookies.js
@@ -1,15 +1,17 @@
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var app = express();
-app.use(cookieParser());
+import express from 'express';
+import cookieParser from 'cookie-parser';
+
+let app = express();
+
+app.use(cookieParser()); // Create req.cookies object
+
 app.get('/', function(req, res) {
   console.log(req.cookies);
-  if (!req.cookies.hasVisited){
-    res.cookie('hasVisited', '1', 
-               { maxAge: 60*60*1000, 
-                 httpOnly: true, 
-                 path:'/'});
+  if (!req.cookies.hasVisited) {
+    res.cookie('hasVisited', '1', { maxAge: 60*60*1000, httpOnly: true, path: '/' });
   }
   res.send("Sending Cookie");
 });
+
 app.listen(80);
+
