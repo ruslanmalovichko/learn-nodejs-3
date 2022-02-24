d6b2a16b9cd42edd4a95840a90b74dd8595af8c8
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2022-02-24 17:40:32 +0200

    Read a book

--- ch19/express_auth_one.js
+++ ch19/express_auth_one.js
@@ -1,13 +1,19 @@
-var express = require('express');
-var basicAuth = require('basic-auth-connect');
-var app = express();
-var auth = basicAuth(function(user, pass) {
+import express from 'express';
+import basicAuth from 'basic-auth-connect';
+
+let app = express();
+
+let auth = basicAuth(function(user, pass) {
   return (user === 'user1' && pass === 'test');
 });
+
 app.get('/library', function(req, res) {
   res.send('Welcome to the library.');
 });
+
 app.get('/restricted', auth, function(req, res) {
   res.send('Welcome to the restricted section.');
 });
+
 app.listen(80);
+
