658e52bba6935d5f82f6e2c212ad94a686aee57c
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2022-02-24 13:50:53 +0200

    Read a book

--- ch19/express_session.js
+++ ch19/express_session.js
@@ -1,25 +1,32 @@
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var cookieSession = require('cookie-session');
-var app = express();
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cookieSession from 'cookie-session';
+
+let app = express();
+
 app.use(cookieParser());
 app.use(cookieSession({secret: 'MAGICALEXPRESSKEY'}));
+
 app.get('/library', function(req, res) {
   console.log(req.cookies);
-  if(req.session.restricted) {
-    res.send('You have been in the restricted section ' + 
-             req.session.restrictedCount + ' times.');
-  }else {
+  if (req.session.restricted) {
+    res.send('You have been in the restricted section ' + req.session.restrictedCount + ' times.');
+  }
+  else {
     res.send('Welcome to the library.');
   }
 });
+
 app.get('/restricted', function(req, res) {
   req.session.restricted = true;
-  if(!req.session.restrictedCount){
-    req.session.restrictedCount = 1;
-  } else {
-    req.session.restrictedCount += 1;
+  if (!req.session.restrictedCount) {
+    req.session.restrictedCount = 1; // Create new session cookies
+  }
+  else {
+    req.session.restrictedCount += 1; // Create new session cookies
   }
   res.redirect('/library');
 });
+
 app.listen(80);
+
