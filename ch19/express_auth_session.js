10a8f01f594f0d3396815e1f68098c85503a3344
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2022-02-26 12:49:57 +0200

    Read a book

--- ch19/express_auth_session.js
+++ ch19/express_auth_session.js
@@ -1,60 +1,86 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
-var crypto = require('crypto');
-function hashPW(pwd){
-  return crypto.createHash('sha256').update(pwd).
-         digest('base64').toString();
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import crypto from 'crypto';
+
+function hashPW(pwd) {
+  return crypto.createHash('sha256').update(pwd).digest('base64').toString();
 }
-var app = express();
+
+let app = express();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser('MAGICString'));
-app.use(session());
-app.get('/restricted', function(req, res){
+
+let sess = {
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}
+
+app.use(session(sess));
+
+app.get('/restricted', function(req, res) {
+  console.log('on restricted page');
+  console.log(req.session.user);
   if (req.session.user) {
-    res.send('<h2>'+ req.session.success + '</h2>' +
-             '<p>You have entered the restricted section<p><br>' +
-             ' <a href="/logout">logout</a>');
-  } else {
+    res.send('<h2>'+ req.session.success + '</h2>' + '<p>You have entered the restricted section<p><br>' + ' <a href="/logout">logout</a>');
+  }
+  else {
     req.session.error = 'Access denied!';
     res.redirect('/login');
   }
 });
-app.get('/logout', function(req, res){
-  req.session.destroy(function(){
+
+app.get('/logout', function(req, res) {
+  req.session.destroy(function() {
     res.redirect('/login');
   });
 });
-app.get('/login', function(req, res){
-  var response = '<form method="POST">' +
+
+app.get('/login', function(req, res) {
+  let response = '<form method="POST">' +
     'Username: <input type="text" name="username"><br>' +
     'Password: <input type="password" name="password"><br>' +
     '<input type="submit" value="Submit"></form>';
-  if(req.session.user){
+  if (req.session.user) {
     res.redirect('/restricted');
-  }else if(req.session.error){
-    response +='<h2>' + req.session.error + '<h2>'; 
+  }
+  else if (req.session.error) {
+    response +='<h2>' + req.session.error + '<h2>';
   }
   res.type('html');
   res.send(response);
 });
-app.post('/login', function(req, res){
+
+app.post('/login', function(req, res) {
   //user should be a lookup of req.body.username in database
-  var user = {name:req.body.username, password:hashPW("myPass")};
+  let user = {name:req.body.username, password:hashPW("myPass")};
+  console.log(user);
+  console.log(user.password);
+  console.log(hashPW(req.body.password.toString()));
+
   if (user.password === hashPW(req.body.password.toString())) {
-    req.session.regenerate(function(){
+    req.session.regenerate(function(err) {
+      console.log('good login');
       req.session.user = user;
       req.session.success = 'Authenticated as ' + user.name;
+      console.log(req.session);
       res.redirect('/restricted');
     });
-  } else {
-    req.session.regenerate(function(){
+  }
+  else {
+    req.session.regenerate(function(err) {
+      console.log('on login page wrong password');
       req.session.error = 'Authentication failed.';
       res.redirect('/restricted');
     });
-    res.redirect('/login');
+
+    // res.redirect('/login');
   }
 });
 app.listen(80);
+
